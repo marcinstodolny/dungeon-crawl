a68762bdc2b150519775cad325429860e27a2dd0
dapie346 <dawid.pietruszewski@gmail.com>
2023-03-22 10:50:07 +0100

    removed redundant lines

--- UI/Display.cs
+++ UI/Display.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Formats.Asn1.AsnWriter;
-
-namespace RoguelikeGame.UI
+﻿namespace RoguelikeGame.UI
 {
     internal class Display
     {
