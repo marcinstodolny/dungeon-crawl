d1096fcaa4ade97b114e58fec90105367278929f
TomaszCitko <citkotomasz@gmail.com>
2023-03-23 14:48:36 +0100

    enums in items updated

--- Items/Useable/Weapons.cs
+++ Items/Useable/Weapons.cs
@@ -9,52 +9,64 @@ namespace RoguelikeGame.Items.Useable
         {
             WeaponType(weaponType);
         }
+        public enum weaponType
+        {
+            PooStick,
+            RustyPan,
+            Club,
+            ShortSword,
+            ShortSAxe,
+            LongSword,
+            LongAxe,
+            Hammer,
+            Claymore
 
-        public void WeaponType(string weaponType)
+        }
+        public void SetWeaponType(weaponType weaponType)
         {
             switch (weaponType)
             {
-                case "PooStick":
+                case weaponType.PooStick:
                     Name = "Ultimate weapon - stick with a poo at the en";
                     MapSymbol = '\u2694';
                     Attack = 500;
                     break;
-                case "RustyPan":
+                case weaponType.RustyPan:
                     Name = "Slightly rusted pan";
                     MapSymbol = '\u2694';
                     Attack = 15;
                     break;
-                case "Club":
+                case weaponType.Club:
                     Name = "Club (Welcome to the club)";
                     MapSymbol = '\u2694';
                     Attack = 20;
                     break;
-                case "ShortSword":
+                case weaponType.ShortSword:
                     Name = "Short sword (looks big in small hands)";
                     MapSymbol = '\u2694';
                     Attack = 30;
                     break;
-                case "ShortSAxe":
+                case weaponType.ShortSAxe:
                     Name = "Short axe";
                     MapSymbol = '\u2694';
                     Attack = 40;
                     break;
-                case "LongSword":
+                case weaponType.LongSword:
                     Name = "Loooong sword";
                     MapSymbol = '\u2694';
                     Attack = 50;
                     break;
-                case "LongAxe":
+                case weaponType.LongAxe:
                     Name = "Long axe";
                     MapSymbol = '\u2694';
                     Attack = 60;
                     break;
-                case "Hammer":
+                case weaponType.Hammer:
                     Name = "Hammer time";
                     MapSymbol = '\u2694';
                     Attack = 65;
                     break;
-                case "Claymore":
+                case weaponType.Claymore:
                     Name = "Claymore";
                     MapSymbol = '\u2694';
                     Attack = 70;
