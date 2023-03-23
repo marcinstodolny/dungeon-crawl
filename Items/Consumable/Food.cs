ec01b82f6e065a81f454906f109a918ac34b427e
Marcin Stodolny <marcin.stodolny@o2.pl>
2023-03-23 14:27:26 +0100

    food and potion as enum

--- Items/Consumable/Food.cs
+++ Items/Consumable/Food.cs
@@ -4,31 +4,39 @@ namespace RoguelikeGame.Items.Consumable
 {
     public class Food : Abstract.Consumable
     {
-        public Food(Square square,string foodType) : base(square, "", ' ', 0)
+        public Food(Square square,FoodType foodType) : base(square, "", ' ', 0)
         {
-            FoodType(foodType);
+            SetFoodType(foodType);
         }
 
-        public void FoodType(string foodType)
+        public enum FoodType
+        {
+            chicken,
+            steak,
+            apple,
+            sushi,
+        }
+
+        public void SetFoodType(FoodType foodType)
         {
             switch (foodType)
             {
-                case "chicken":
+                case FoodType.chicken:
                     Name = "Dungeon chicken wings";
                     MapSymbol = '\u10EC';
                     HPrestore = 150;
                     break;
-                case "steak":
+                case FoodType.steak:
                     Name = "Perfectly good steak found on the floor";
                     MapSymbol = '\u10EC';
                     HPrestore = 250;
                     break;
-                case "apple":
+                case FoodType.apple:
                     Name = "Apple (an apple a day, keeps the doctor away)";
                     MapSymbol = '\u10EC';
                     HPrestore = 20;
                     break;
-                case "sushi":
+                case FoodType.sushi:
                     Name = "Sushi mix set no.23 (I wonder who ordered it)";
                     MapSymbol = '\u10EC';
                     HPrestore = 150;
--- Items/Consumable/Potions.cs
+++ Items/Consumable/Potions.cs
@@ -4,31 +4,39 @@ namespace RoguelikeGame.Items.Consumable
 {
     public class Potions : Abstract.Consumable
     {
-        public Potions(Square square, string foodType) : base(square, "", ' ', 0)
+        public Potions(Square square, PotionType foodType) : base(square, "", ' ', 0)
         {
-            PotionType(foodType);
+            SetPotionType(foodType);
         }
 
-        public void PotionType(string potionType)
+        public enum PotionType
+        {
+            EnemiesTears,
+            EnemiesBlood,
+            GinAndTonic,
+            DMT,
+        }
+
+        public void SetPotionType(PotionType potionType)
         {
             switch (potionType)
             {
-                case "EnemiesTears":
+                case PotionType.EnemiesTears:
                     Name = "Tears of your enemies";
                     MapSymbol = '\u104E';
                     HPrestore = 150;
                     break;
-                case "EnemiesBlood":
+                case PotionType.EnemiesBlood:
                     Name = "Blood of your enemies";
                     MapSymbol = '\u104E';
                     HPrestore = 200;
                     break;
-                case "GinAndTonic":
+                case PotionType.GinAndTonic:
                     Name = "Gin & Tonic";
                     MapSymbol = '\u104E';
                     HPrestore = 50;
                     break;
-                case "DMT":
+                case PotionType.DMT:
                     Name = "Dimetylotryptamina";
                     MapSymbol = '\u104E';
                     HPrestore = 1000;
