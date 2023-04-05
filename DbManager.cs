5cf40d033f60178439d7f1e8fa28f5a3eab5b2b9
TomaszCitko <citkotomasz@gmail.com>
2023-04-05 13:58:24 +0200

    AddItemToDatabase

--- DbManager.cs
+++ DbManager.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using System.ComponentModel;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
 using System.Configuration;
 using System.Data;
 using System.Runtime.CompilerServices;
@@ -116,9 +118,36 @@ public class DbManager
             throw new RuntimeWrappedException(e);
         }
 
-        public static Dictionary<string, string> AddItemToDatabase()
+        public static void AddItemToDatabase<T>(T item)
         {
-            throw new NotImplementedException();
+            const string getIdCommand = $"SELECT id FROM {item.GetType().Name} WHERE Name = {item.Name};";
+
+            const string insertItemCommand = @"INSERT INTO SAVE_Inventory (Item_Type, Item_Id)
+                            VALUES (@Item_Type, @Item_Id);";
+            
+            try
+            {
+                using (var connection = new SqlConnection(ConnectionString))
+                {
+                    var cmdGet = new SqlCommand(getIdCommand, connection);
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+                    var data = cmdGet.ExecuteReader();
+                    var itemId = data.GetInt32("Id");
+                    connection.Close();
+
+                    var cmdInsert = new SqlCommand(insertItemCommand, connection);
+                    if (connection.State == ConnectionState.Closed)
+                        connection.Open();
+                    cmdInsert.Parameters.AddWithValue("@Item_Type", item.GetType().Name);
+                    cmdInsert.Parameters.AddWithValue("@Item_Id", itemId);
+                    connection.Close();
+                }
+            }
+            catch (SqlException e)
+            {
+                throw new RuntimeWrappedException(e);
+            }
         }
 
         public static Dictionary<string, string> RemoveItemFromDatabase()
