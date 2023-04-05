dfded91668b39486ff33169bc16252832cda3f6c
TomaszCitko <citkotomasz@gmail.com>
2023-04-05 10:48:26 +0200

    Query updated with save

--- Data/database_querry.sql
+++ Data/database_querry.sql
@@ -6,6 +6,14 @@ DROP TABLE IF EXISTS Weapons
 DROP TABLE IF EXISTS Allies
 DROP TABLE IF EXISTS Enemies
 
+DROP TABLE IF EXISTS SAVE_Player
+DROP TABLE IF EXISTS SAVE_Room
+DROP TABLE IF EXISTS SAVE_RoomCorners
+DROP TABLE IF EXISTS SAVE_Doors
+DROP TABLE IF EXISTS SAVE_MapItems
+DROP TABLE IF EXISTS SAVE_Inventory
+DROP TABLE IF EXISTS SAVE_Monsters
+
 
 CREATE TABLE [dbo].[Armors] (
     [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
@@ -59,6 +67,54 @@ CREATE TABLE [dbo].[Enemies]
 	[Symbol] NCHAR(10) NULL,
     [Health] INT NULL, 
     [Damage] INT NULL 
+);
+
+CREATE TABLE [dbo].[SAVE_Player] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Coord.X] INT NULL,
+    [Coord.Y] INT NULL,
+    [Armor]  INT NULL,
+    [HP] INT NULL,
+    [Damage]  INT NULL,
+    [DMT] BIT NULL,
+);
+CREATE TABLE [dbo].[SAVE_Room] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Corner_NW_id] INT NULL,
+    [Corner_NE_id] INT NULL,
+    [Corner_SW_id] INT NULL,
+    [Corner_SE_id] INT NULL,
+    [Visibility] BIT NULL,
+);
+CREATE TABLE [dbo].[SAVE_RoomCorners] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Room_id] INT NULL,
+    [Coord.X] INT NULL,
+    [Coord.Y] INT NULL,
+);
+CREATE TABLE [dbo].[SAVE_Doors] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Room_id] INT NULL,
+    [Coord.X] INT NULL,
+    [Coord.Y] INT NULL,
+    [Opened] BIT NULL,
+);
+CREATE TABLE [dbo].[SAVE_MapItems] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Item_Id] INT NULL,
+    [Coord.X] INT NULL,
+    [Coord.Y] INT NULL,
+);
+CREATE TABLE [dbo].[SAVE_Monsters] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Monster_Id] INT NULL,
+    [Coord.X] INT NULL,
+    [Coord.Y] INT NULL,
+);
+
+CREATE TABLE [dbo].[SAVE_Inventory] (
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Item_Id] INT NULL,
 );
 
 
